6c1f68f95ba8a2bdd294a9b7b119f8e035bd9a64
David Robins <dave.a.robins@gmail.com>
2020-07-26 10:21:16 -0500

    fixed fetch

--- random.js
+++ random.js
@@ -1,5 +1,4 @@
 const races = ["human", "elf", "dwarf", "orc", "halfling"]
-const apiRaces = fetchApi("races")
 const classes = ["fighter", "rogue", "wizard", "cleric", "bard"]
 const images = ["Dwarf Bard.jpg","Dwarf Cleric.jpg", "Dwarf Fighter.png", "Dwarf Rogue.png", "Dwarf Wizard.jpg",
 "Elf Bard.jpg", "Elf Cleric.jpg", "Elf Fighter.png", "Elf Rogue.jpg", "Elf Wizard.png", 
@@ -10,92 +9,92 @@ const  attributes= [6,7,8,9,10,11,12,13,14,15,16,17,18]
 const level = 2
 
 function imgSel(race,clas) {
-    if (race==="dwarf"){
-        if (clas==="bard") {
+    if (race==="Dwarf"){
+        if (clas==="Bard") {
             return images[0]
             }
-        if (clas==="cleric") {
+        if (clas==="Cleric") {
             return images[1]
             }
-        if (clas==="fighter") {
+        if (clas==="Fighter") {
             return images[2]
             }
-        if (clas==="rogue") {
+        if (clas==="Rogue") {
             return images[3]
             }
-        if (clas==="wizard") {
+        if (clas==="Wizard") {
             return images[4]
             }
         }
 
-        if (race==="elf"){
-        if (clas==="bard") {
+        if (race==="Elf"){
+        if (clas==="Bard") {
             return images[5]
                 }
-        if (clas==="cleric") {
+        if (clas==="Cleric") {
             return images[6]
             }
-        if (clas==="fighter") {
+        if (clas==="Fighter") {
             return images[7]
             }
-        if (clas==="rogue") {
+        if (clas==="Rogue") {
             return images[8]
             }
-        if (clas==="wizard") {
+        if (clas==="Wizard") {
             return images[9]
             }
         }
     
-    if (race==="halfling"){
-        if (clas==="bard") {
+    if (race==="Halfling"){
+        if (clas==="Bard") {
             return images[10]
         }
-        if (clas==="cleric") {
+        if (clas==="Cleric") {
             return images[11]
         }
-        if (clas==="fighter") {
+        if (clas==="Fighter") {
             return images[12]
         }
-        if (clas==="rogue") {
+        if (clas==="Rogue") {
             return images[13]
         }
-        if (clas==="wizard") {
+        if (clas==="Wizard") {
             return images[14]
         }
     }
 
-    if (race==="human"){
-        if (clas==="bard") {
+    if (race==="Human"){
+        if (clas==="Bard") {
             return images[15]
         }
-        if (clas==="cleric") {
+        if (clas==="Cleric") {
             return images[16]
         }
-        if (clas==="fighter") {
+        if (clas==="Fighter") {
             return images[17]
         }
-        if (clas==="rogue") {
+        if (clas==="Rogue") {
             return images[18]
         }
-        if (clas==="wizard") {
+        if (clas==="Wizard") {
             return images[19]
         }
     }
 
-    if (race==="orc"){
-    if (clas==="bard") {
+    if (race==="Orc"){
+    if (clas==="Bard") {
         return images[20]
         }
-    if (clas==="cleric") {
+    if (clas==="Cleric") {
         return images[21]
         }
-    if (clas==="fighter") {
+    if (clas==="Fighter") {
         return images[22]
         }
-    if (clas==="rogue") {
+    if (clas==="Rogue") {
         return images[23]
         }
-    if (clas==="wizard") {
+    if (clas==="Wizard") {
         return images[24]
         }
     }
@@ -106,16 +105,16 @@ function getRandom(array) {
     return array[random]
 }
 
-function myFunc() {
-    const race= getRandom(races)
-    const clas= getRandom(classes)
-
-    document.getElementById("proficiency").value = level;
+async function myFunc() {
+    const apiRaces = await apiAsync('races')
+    const apiClasses = await apiAsync('classes')
     
+    const race= getRandom(apiRaces.results).name
+    const clas= getRandom(apiClasses.results).name
+    
+    document.getElementById("proficiency").value = level;
     document.getElementById("character").innerHTML = race;
-
     document.getElementById("job").innerHTML = clas;
-
     document.getElementById('divImage').innerHTML=
         `<img src="Dudes/${imgSel(race, clas)}" width="200" height="200">`;
 
@@ -152,16 +151,6 @@ function modifiers(attributes) {
     profSaveDex.value += 2;
 }
 
-function fetchApi(randomApiUrl) {
-    fetch(`https://www.dnd5eapi.co/api/${randomApiUrl}`).then(function (response) {
-        // The API call was successful!
-        return response.json();
-    }).then(function (data) {
-        // This is the JSON from our response
-        console.log(data.results[0].name);
-        return data.results
-    }).catch(function (err) {
-        // There was an error
-        console.warn('Something went wrong.', err);
-    });
+async function apiAsync(apiUrl) {
+    return fetch(`https://www.dnd5eapi.co/api/${apiUrl}`).then(res => res.json())
 }
