4ada38c1c320da6791e67fed3e1dffa741a39111
David Robins <dave.a.robins@gmail.com>
2020-08-16 17:56:10 -0500

    finished inventory, added to seperate file

--- CharacterCore.js
+++ CharacterCore.js
@@ -8,17 +8,13 @@ async function myFunc() {
     const apiClasses = await apiAsync('classes')
     // List of Proficiencies
     const languages= raceDetails.languages.map(lan => lan.name)
-    
     const traits= raceDetails.traits.map(raceTrait => raceTrait.name)
     const clas= getRandom(apiClasses.results)
-    
     const classDetails = await apiAsync(`classes/${clas.index}`)
-    
     const proficiencies= classDetails.proficiencies.map(prof => prof.name)
 
     //here buildInventory returns full item Objects, since we'll probably use the URLs later
     //inventoryText is unwrapping the name and the quantity
-    console.log("class: ", clas)
     const inventory = await buildInventory(classDetails.starting_equipment.url)
     const inventoryText = inventory.map(item => `${item.quantity || 1} ${item.equipment.name}`)
     
@@ -43,60 +39,6 @@ async function myFunc() {
     generateAttributes()   
 }
 
-async function buildInventory(startingEquipmentURL) {
-    const chosenInventory = []
-    const fullInventory = await apiAsync(startingEquipmentURL.substring(5))
-    fullInventory.starting_equipment.map(item => {
-        console.log("pushed 0 ", item.equipment.name)
-        chosenInventory.push(item) //basic equipment
-    })
-    // const inventoryAsArray = Object.values(fullInventory)   //just converting from an Object to an Array
-    const choices = await makeChoices(fullInventory.starting_equipment_options)
-    choices.map(choice => chosenInventory.push(choice))
-
-    return chosenInventory.flat()
-}
-
-async function makeChoices(options) {
-    const allthethings = await Promise.all( 
-        options.map(async choiceArray => {
-            if (choiceArray.from.equipment_category) {
-                const callResult = await apiAsync(choiceArray.from.equipment_category.url.substring(5))
-                const item = {
-                    equipment: getRandom(callResult.equipment),
-                    quantity: 1
-                }
-                console.log("pushed 1 ", item.equipment.name)
-                return item
-            }
-
-            const choice = getRandom(choiceArray.from)
-            if (Array.isArray(choice)) { return choice }
-
-            if (choice.equipment){
-                console.log("pushed 2 ", choice.equipment.name)
-                return choice
-            } else if (choice.equipment_option) {
-                if (choice.equipment_option.from.equipment_category) {
-                    const callResult = await apiAsync(choice.equipment_option.from.equipment_category.url.substring(5))
-                    const item = {
-                        equipment: getRandom(callResult.equipment),
-                        quantity: 1
-                    }
-                    console.log("pushed 3 ", item.equipment.name)
-                    return item
-                } else if (Array.isArray(choice.equipment_option.from)){
-                    const returnedChoice = getRandom(choice.equipment_option.from)
-                    console.log("pushed 4 ", returnedChoice.equipment.name)
-                    return returnedChoice
-                }
-            }
-        })
-    )
-    console.log("make choices returns: ", allthethings)
-    return allthethings
-}
-
-async function apiAsync(apiUrl) {
+function apiAsync(apiUrl) {
     return fetch(`https://www.dnd5eapi.co/api/${apiUrl}`).then(res => res.json())
 }
